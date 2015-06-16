12a5420927d7e11b221e794a54969288b640a18b
Henare Degan <henare.degan@gmail.com>
2015-06-16 16:22:42 +1000

    Write scraper for Botany Bay

--- scraper.rb
+++ scraper.rb
@@ -1,25 +1,28 @@
-# This is a template for a Ruby scraper on morph.io (https://morph.io)
-# including some code snippets below that you should find helpful
+require 'scraperwiki'
+require 'mechanize'
 
-# require 'scraperwiki'
-# require 'mechanize'
-#
-# agent = Mechanize.new
-#
-# # Read in a page
-# page = agent.get("http://foo.com")
-#
-# # Find somehing on the page using css selectors
-# p page.at('div.content')
-#
-# # Write out to the sqlite database using scraperwiki library
-# ScraperWiki.save_sqlite(["name"], {"name" => "susan", "occupation" => "software developer"})
-#
-# # An arbitrary query against the database
-# ScraperWiki.select("* from data where 'name'='peter'")
+agent = Mechanize.new
 
-# You don't have to do things with the Mechanize or ScraperWiki libraries.
-# You can use whatever gems you want: https://morph.io/documentation/ruby
-# All that matters is that your final data is written to an SQLite database
-# called "data.sqlite" in the current working directory which has at least a table
-# called "data".
+base_url = "http://210.8.51.75/"
+comment_url = "mailto:council@botanybay.nsw.gov.au"
+page = agent.get("#{base_url}eservice/daEnquiry/currentlyAdvertised.do?function_id=521&nodeNum=2812")
+
+page.search(".non_table_headers").each do |header|
+  field_values = header.next.search(".inputField").map { |i| i.inner_text }
+
+  record = {
+    address: header.inner_text.squeeze(" "),
+    description: field_values[0],
+    council_reference: field_values[3],
+    date_received: Date.parse(field_values[4]).to_s,
+    info_url: base_url + header.at(:a).attr(:href),
+    comment_url: comment_url,
+    date_scraped: Date.today
+  }
+
+  if (ScraperWiki.select("* from data where `council_reference`='#{record[:council_reference]}'").empty? rescue true)
+    ScraperWiki.save_sqlite([:council_reference], record)
+  else
+    puts "Skipping already saved record " + record[:council_reference]
+  end
+end
